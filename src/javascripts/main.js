755cd39feb429bf1d98231e1b5412c37503694cc
Jennifer Lipps <jjlipps@gmail.com>
2019-04-20 11:33:42 -0500

    added locations data

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,8 +2,11 @@ import '../styles/main.scss';
 
 import movies from './components/movies/movies';
 
+import locations from './components/locations/locations';
+
 const init = () => {
   movies.initializeMovies();
+  locations.initializeLocations();
 };
 
 init();
